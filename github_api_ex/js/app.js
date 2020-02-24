e5c54f59883f029016e5ac095d522bff2e68ed61
Ewerthon Silva <39033581+EwerthonDev@users.noreply.github.com>
2020-02-24 16:12:01 -0300

    Add the project

---
+++ github_api_ex/js/app.js
@@ -0,0 +1,25 @@
+var ElementUsername = document.querySelector("#busca input[name=username]");
+var ElementSearch = document.querySelector("#busca button[name=buscar]");
+var ElementResponse = document.querySelector("#res .elementos_resposta");
+ElementSearch.setAttribute("onclick", "buscar()");
+ElementUsername.value = '';
+function buscar() {
+    var conexao = new XMLHttpRequest();
+    var username = ElementUsername.value;
+    conexao.open('GET', `https://api.github.com/users/${username}`);
+    conexao.send(null);
+    conexao.onreadystatechange = function () {
+        if (conexao.readyState === 4) {
+            var perfil = JSON.parse(conexao.responseText);
+            ElementResponse.innerHTML = `
+            <p class="user"> <b>${perfil.login}</b></p>
+            <p class="name"><b>Nome</b> 👉 ${perfil.name}</p>
+            <p class="bio"><b>Bio</b> 👉 ${perfil.bio}</p>
+            <figure><img clas="img" src="${perfil.avatar_url}"></figure>
+            <p clas="local"><b>Local</b> 👉 ${perfil.location}</p>
+            <p class="conta"><b>Conta criada em</b> 👉 ${perfil.created_at}</p>
+            `
+            ElementUsername.value = '';
+        }
+    }
+}
